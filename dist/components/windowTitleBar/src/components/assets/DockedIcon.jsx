3c4e31ea03ad4f74cbbe85019725927465d55190
Pierre de Leastar <pdeleastar@gmail.com>
2019-01-25 12:10:24 +0100

    production manifest added

--- dist/components/windowTitleBar/src/components/assets/DockedIcon.jsx
+++ dist/components/windowTitleBar/src/components/assets/DockedIcon.jsx
@@ -7,7 +7,7 @@ export const DockedIcon = () => (
 		height="12"
 		viewBox="0 0 12 12"
 	>
-		<g fill="none" fill-rule="evenodd">
+		<g fill="none" fillRule="evenodd">
 			<path stroke="#93E4C3" d="M.5.5h11v11H.5z" />
 			<path fill="#93E4C3" d="M2 2h8v8H2z" />
 		</g>
--- dist/components/windowTitleBar/src/components/assets/UndockedIcon.jsx
+++ dist/components/windowTitleBar/src/components/assets/UndockedIcon.jsx
@@ -9,7 +9,7 @@ export const UndockedIcon = shade => {
 			height="12"
 			viewBox="0 0 13 12"
 		>
-			<g fill="none" fill-rule="nonzero">
+			<g fill="none" fillRule="nonzero">
 				<path stroke={hex} d="M6.5.5h6v6h-6zM.5 5.5h6v6h-6z" />
 				<path fill={hex} stroke="#3D4455" d="M2.5 2.5h8v8h-8z" />
 			</g>
