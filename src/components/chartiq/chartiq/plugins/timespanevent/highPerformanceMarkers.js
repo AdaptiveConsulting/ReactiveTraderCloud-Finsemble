dc4f24123c67b5978006c6137746168253ca020a
Justin Rothweiler <36375152+jrothweiler@users.noreply.github.com>
2020-12-18 18:30:14 -0500

    ARTP-1365: Add upgraded currency chart (#34)

---
+++ src/components/chartiq/chartiq/plugins/timespanevent/highPerformanceMarkers.js
@@ -0,0 +1,24 @@
+/**
+ *	8.1.0
+ *	Generation date: 2020-12-14T16:21:56.817Z
+ *	Client name: finsemble
+ *	Package Type: Technical Analysis
+ *	License type: annual
+ *	Expiration date: "2021/07/20"
+ *	Domain lock: ["127.0.0.1","localhost","chartiq.com","finsemble.com"]
+ *	iFrame lock: true
+ */
+
+/***********************************************************
+ * Copyright by ChartIQ, Inc.
+ * Licensed under the ChartIQ, Inc. Developer License Agreement https://www.chartiq.com/developer-license-agreement
+*************************************************************/
+/*************************************** DO NOT MAKE CHANGES TO THIS LIBRARY FILE!! **************************************/
+/* If you wish to overwrite default functionality, create a separate file with a copy of the methods you are overwriting */
+/* and load that file right after the library has been loaded, but before the chart engine is instantiated.              */
+/* Directly modifying library files will prevent upgrades and the ability for ChartIQ to support your solution.          */
+/*************************************************************************************************************************/
+/* eslint-disable no-extra-parens */
+
+
+// This file is a placeholder and is required.  Do not delete it!
