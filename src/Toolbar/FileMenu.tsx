1978714ce801187cb179aafed9f2a9f6728a4b5b
Moise Lubwimi <herr_moise@hotmail.com>
2023-09-19 11:31:42 +0100

    run prettier

--- src/Toolbar/FileMenu.tsx
+++ src/Toolbar/FileMenu.tsx
@@ -1,32 +1,32 @@
 import {
-	CentralLogger,
-	Documentation,
-	Menu,
-	Preferences,
-	Quit,
-	Reset,
-	Restart,
-	SystemLog,
+  CentralLogger,
+  Documentation,
+  Menu,
+  Preferences,
+  Quit,
+  Reset,
+  Restart,
+  SystemLog,
 } from '@finsemble/finsemble-core'
 import React from 'react'
 
 export const FileMenu = () => (
-	<Menu
-		id="fileMenu"
-		title={
-			<img className="finsemble-toolbar-brand-logo" src="../../assets/img/adaptive-logo.png" />
-		}
-	>
-		<Preferences />
-		<SystemLog />
-		<CentralLogger />
-		<Documentation />
-		<Restart />
-		<Reset />
-		<Quit />
-		{/* To add your own items to the menu, import MenuItem from
-		 * "@finsemble/finsemble-ui/react/components" and add the following:
-		 * <MenuItem onClick={...}>Your Item</MenuItem>
-		 */}
-	</Menu>
+  <Menu
+    id="fileMenu"
+    title={
+      <img className="finsemble-toolbar-brand-logo" src="../../assets/img/adaptive-logo.png" />
+    }
+  >
+    <Preferences />
+    <SystemLog />
+    <CentralLogger />
+    <Documentation />
+    <Restart />
+    <Reset />
+    <Quit />
+    {/* To add your own items to the menu, import MenuItem from
+     * "@finsemble/finsemble-ui/react/components" and add the following:
+     * <MenuItem onClick={...}>Your Item</MenuItem>
+     */}
+  </Menu>
 )
